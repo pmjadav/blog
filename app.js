a94d4abd02661489d9bbf37a706f551b619f9dc2
Prashant Jadav <pmjadav@gmail.com>
2020-02-11 05:15:57 +0530

    stable version on 11/02/2020 at 5:15 a.m.

--- app.js
+++ app.js
@@ -2,41 +2,66 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const ejs = require('ejs')
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost')
 
-app.use(express.static('public'));
-app.use('/bs4', express.static(path.resolve(__dirname, '/node_modules/bootstrap/dist/css')));
-app.set('view engine', 'ejs')
+// parse application/json
+app.use(bodyParser.json())
 
 // parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+app.use('/bs4css', express.static(path.resolve(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/bs4js', express.static(path.resolve(__dirname, '/node_modules/bootstrap/dist/js')));
+
+app.set('view engine', 'ejs')
 
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser : true} )
 
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
    //res.sendFile( path.resolve(__dirname, "index.html") );
    res.render('index')
 })
-app.get('/about', function (req, res) {
+app.get('/about',  (req, res) => {
    //res.sendFile( path.resolve(__dirname, "about.html") );
    res.render('about')
 })
-app.get('/contact', function (req, res) {
+
+app.get('/posts/new',  (req, res) => {
+   //res.sendFile( path.resolve(__dirname, "create.html") );
+   res.render('create')
+})
+
+app.get('/contact',  (req, res) => {
    //res.sendFile( path.resolve(__dirname, "contact.html") );
    res.render('contact')
 })
 
-app.get('/index.html', function (req, res) {
+app.get('/index.html',  (req, res) => {
    //res.sendFile( path.resolve(__dirname, "index.html") );
    res.render('index')
 })
 
-app.post('/addname', function (req, res) {
+app.post('/posts/store', async (req, res) => {
+   // console.log(req.body)
+   // res.redirect('/')
+   //res.end(JSON.stringify(req.body))
+
+   // BlogPost.create(req.body, (err, post) => {
+   //    res.redirect('/')
+   // })
+   await BlogPost.create(req.body) //synchronous call
+   res.redirect('/')
+})
+
+app.post('/addname', (req, res) => {
    response = {
       first_name:	req.body.first_name,
       last_name:	req.body.last_name
    };
+   console.log(req.body.first_name)
    res.end(JSON.stringify(response));
 })
 
