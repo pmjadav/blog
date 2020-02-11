8340c9149180863acdadee1a6a06155585fa3f2d
Prashant Jadav <pmjadav@gmail.com>
2020-02-11 23:46:30 +0530

    commit on 11-Feb-2020 at 23:46 p.m.

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+// const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema;
 
@@ -6,6 +7,14 @@ const BlogPostSchema = new Schema( {
 	title: String,
 	body: String
 });
+
+// BlogPostSchema.pre('save', function(next) {
+// 	const blog = this
+// 	bcrypt.hash(blog.body, 10, (err, hash) => {
+// 		blog.body = hash
+// 		next()
+// 	})
+// })
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 
